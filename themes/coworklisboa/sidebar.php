f2f31590196f5c0bf9cfb07f763319afa5f36fee
Nuno Morgadinho <nuno.morgadinho@gmail.com>
2013-03-21 11:26:21 +0000

    sidebar

--- themes/coworklisboa/sidebar.php
+++ themes/coworklisboa/sidebar.php
@@ -9,28 +9,7 @@
 
 <div class="container_side_right">
 		<div id="secondary" class="widget-area" role="complementary">
-			<!-- <?php do_action( 'before_sidebar' ); ?>
-			<?php if ( ! dynamic_sidebar( 'sidebar-1' ) ) : ?>
-
-				<aside id="search" class="widget widget_search">
-					<?php get_search_form(); ?>
-				</aside>
-
-				<aside id="archives" class="widget">
-					<h1 class="widget-title"><?php _e( 'Archives', 'rec' ); ?></h1>
-					<ul>
-						<?php wp_get_archives( array( 'type' => 'monthly' ) ); ?>
-					</ul>
-				</aside>
-
-				<aside id="meta" class="widget">
-					<h1 class="widget-title"><?php _e( 'Meta', 'rec' ); ?></h1>
-					<ul>
-						<?php wp_register(); ?>
-						<li><?php wp_loginout(); ?></li>
-						<?php wp_meta(); ?>
-					</ul>
-				</aside> -->
+			
 
 				<a href="http://www.zonempresas.pt/" target="_blank"><img src="<?php echo get_bloginfo('stylesheet_directory'); ?>/images/zon.png"></a>
 
